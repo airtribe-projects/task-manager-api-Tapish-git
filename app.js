fc3548c74038a08cbfbc9fb9dbd110dd124d349b
Tapish Bagdi <tapishbagdi@gmail.com>
2026-02-24 18:43:28 +0530

    created and tested post api

--- app.js
+++ app.js
@@ -25,9 +25,21 @@ app.get('/api/v1/tasks/:id', (req, res) => {
     const task = tasks.find(task => task.id === taskId);
     if(!task) {
         return res.status(404).json({ error: 'Task not found!!' });
-    }
-    
+    }    
     res.status(200).json(task);
+})
+
+// POST /tasks: Create a new task with the required fields (title, description, completed)
+app.post('/api/v1/tasks', (req, res) => {
+    const { title, description, completed } = req.body;
+    const newTask = {
+        id: tasks.length + 1,
+        title,
+        description,
+        completed
+    }
+    tasks.push(newTask);
+    res.status(201).json({ message: "task created successfully", task: newTask });
 })
 
 
