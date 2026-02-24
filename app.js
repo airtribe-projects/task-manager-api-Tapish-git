87c16160e4802010c200bfb9e38331cfff679d8e
Tapish Bagdi <tapishbagdi@gmail.com>
2026-02-24 19:04:23 +0530

    added put api

--- app.js
+++ app.js
@@ -42,6 +42,27 @@ app.post('/api/v1/tasks', (req, res) => {
     res.status(201).json({ message: "task created successfully", task: newTask });
 })
 
+// PUT /tasks/:id: Update an existing task by its ID
+app.put('/api/v1/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+    if (taskIndex === -1) {
+        return res.status(404).json({ error: 'Task not found!!' });
+    }
+
+    const {title, description, completed} = req.body;
+    const updatedTask = {
+        id: taskId,
+        title: title || tasks[taskIndex].title,
+        description: description || tasks[taskIndex].description,
+        completed: completed !== undefined ? completed : tasks[taskIndex].completed
+    }
+    tasks[taskIndex] = updatedTask;
+
+    res.status(200).json(updatedTask);
+})
+
 
 app.listen(port, (err) => {
     if (err) {
